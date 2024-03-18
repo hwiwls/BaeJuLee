b995240b75d05cdfa26fe9470c40bf9a1f812475
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-03-19 01:12:17 +0900

    fix: date picker가 안 보이는 버그 해결, refactor: 날짜 선택하는 UI를 AddMealDateView로 빼기

--- BaeJuLee/Source/Presentation/AddMeal/ViewController/AddMealViewController.swift
+++ BaeJuLee/Source/Presentation/AddMeal/ViewController/AddMealViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 import Then
 
-final class AddMealViewController: BaseViewController, UITextFieldDelegate {
+final class AddMealViewController: BaseViewController {
     enum MealTime: String {
         case breakfast = "아침"
         case lunch = "점심"
@@ -25,40 +25,17 @@ final class AddMealViewController: BaseViewController, UITextFieldDelegate {
     
     var viewModel = AddMealViewModel()
     
+    private let addMealDateView = AddMealDateView()
+    
     private let addMealDetailView = AddMealDetailView()
-
     
-    let dateView = UIView().then {
-        $0.backgroundColor = .white
-        $0.layer.cornerRadius = 10
-    }
-    
-    let dateLabel = UILabel().then {
-        let dateString = DateFormatterUtility.shared.string(from: Date(), withFormat: "yyyy년 MM월 dd일")
-        $0.text = dateString
-        $0.textColor = .black
-        $0.font = .systemFont(ofSize: 16)
-    }
+    // 편집 중인 텍스트 필드: dateTextField 입력시 완료 버튼 활성화를 피하려고
+    private var activeTextField: UITextField?
     
     let datePicker = UIDatePicker().then {
         $0.datePickerMode = .date
         $0.preferredDatePickerStyle = .wheels
         $0.locale = Locale(identifier: "ko-KR")
-    }
-    
-    let dateChevronBtn = UIButton().then {
-        $0.setImage(UIImage(systemName: "chevron.down")?.withRenderingMode(.alwaysOriginal).withTintColor(.pointRegularLightGray), for: .normal)
-    }
-    
-    let mealTimeLabel = UILabel().then {
-        $0.text = "식사 시간(필수)"
-        $0.textColor = .black
-        $0.font = .systemFont(ofSize: 16)
-        $0.textAlignment = .left
-    }
-    
-    let mealView = UIView().then {
-        $0.backgroundColor = .white
     }
 
     let completeButton = UIButton().then {
@@ -73,27 +50,154 @@ final class AddMealViewController: BaseViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         bindViewModel()
-        configureTapGesture()
         setupInitialSelection()
         addKeyboardObservers()
-            setupTapGesture()
-            setupToolbar()
+        setupTapGesture()
+        setupToolbar()
+        setupDatePickerAndToolbar()
     }
+    
+    
+    
+    private func setupInitialSelection() {
+        viewModel.inputMealTimeSelected.value = .breakfast
+        viewModel.inputMealTypeSelected.value = .homeCooked
+        viewModel.inputMealPriceEntered.value = false
+            
+        
+        addMealDetailView.updateButtonStyles(selectedMealTime: viewModel.inputMealTimeSelected.value, selectedMealType: viewModel.inputMealTypeSelected.value)
+        
+    }
+    
+    
+    private func bindViewModel() {
+        viewModel.outputFormattedDate.bind { [weak self] formattedDate in
+            guard let self = self, let formattedDate = formattedDate else { return }
+            self.addMealDateView.dateTextField.text = formattedDate
+        }
+        
+        viewModel.outputIsCompleteButtonEnabled.bind { [weak self] isEnabled in
+            self?.completeButton.isEnabled = isEnabled
+            self?.completeButton.backgroundColor = isEnabled ? .pointGreen : .superLightGray
+            self?.completeButton.setTitleColor(isEnabled ? .white : .gray, for: .normal)
+        }
+        
+        addMealDetailView.breakfastButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
+        addMealDetailView.lunchButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
+        addMealDetailView.dinnerButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
+        addMealDetailView.snackButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
+
+        addMealDetailView.homeCookedButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
+        addMealDetailView.deliveryButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
+        addMealDetailView.diningOutButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
+        
+    }
+    
+
+   
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField == addMealDetailView.mealPriceTextField {
+            guard let text = textField.text else {
+                viewModel.inputMealPriceEntered.value = false
+                return
+            }
+            viewModel.inputMealPriceEntered.value = !text.isEmpty
+        }
+    }
+    
+   
+    override func configView() {
+        addMealDateView.dateTextField.delegate = self
+        
+        addMealDetailView.mealPriceTextField.delegate = self
+        addMealDetailView.mealNameTextField.delegate = self
+        
+        addMealDetailView.mealPriceTextField.addTarget(self, action: #selector(textFieldDidChangeSelection), for: .editingChanged)
+        
+        completeButton.addTarget(self, action: #selector(saveMealData), for: .touchUpInside)
+    }
+
+    
+    @objc private func saveMealData() {
+        viewModel.saveMealData(mealName: addMealDetailView.mealNameTextField.text, mealPrice: addMealDetailView.mealPriceTextField.text)
+//        self.navigationController?.popViewController(animated: true)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func configHierarchy() {
+        view.addSubviews([
+            addMealDateView,
+            addMealDetailView,
+            completeButton
+        ])
+    }
+    
+    override func configLayout() {
+        addMealDateView.snp.makeConstraints {
+            $0.height.equalTo(60)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(36)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        addMealDetailView.snp.makeConstraints {
+            $0.height.equalTo(450)
+            $0.top.equalTo(addMealDateView.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        completeButton.snp.makeConstraints {
+            $0.leading.bottom.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(52)
+        }
+        
+        
+    }
+
+}
+
+extension AddMealViewController {
+    func setupDatePickerAndToolbar() {
+            // DatePicker 설정
+            datePicker.addTarget(self, action: #selector(datePickerChanged), for: .valueChanged)
+
+            // Toolbar 설정
+            let toolbar = UIToolbar()
+            toolbar.sizeToFit()
+            let doneButton = UIBarButtonItem(title: "확인", style: .plain, target: self, action: #selector(donePressed))
+            let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            toolbar.setItems([flexibleSpace, doneButton], animated: true)
+
+            // dateTextField 입력 방식 변경
+        addMealDateView.dateTextField.inputAccessoryView = toolbar
+        addMealDateView.dateTextField.inputView = datePicker
+        }
+
+        @objc func datePickerChanged() {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+            addMealDateView.dateTextField.text = dateFormatter.string(from: datePicker.date)
+        }
+
+        @objc func donePressed() {
+            // '확인' 버튼을 누를 때 실행될 로직
+            datePickerChanged() // 날짜 업데이트
+            view.endEditing(true) // 키보드(또는 DatePicker) 숨기기
+        }
     
     private func addKeyboardObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
     @objc func keyboardWillShow(notification: NSNotification) {
         guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
             return
         }
-        if self.view.frame.origin.y == 0 {
+        if let activeTextField = activeTextField, (activeTextField == addMealDetailView.mealPriceTextField || activeTextField == addMealDetailView.mealNameTextField), self.view.frame.origin.y == 0 {
             self.view.frame.origin.y -= keyboardSize.height / 2
         }
     }
-
+    
     @objc func keyboardWillHide(notification: NSNotification) {
         if self.view.frame.origin.y != 0 {
             self.view.frame.origin.y = 0
@@ -104,7 +208,7 @@ final class AddMealViewController: BaseViewController, UITextFieldDelegate {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
     }
-
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -125,57 +229,23 @@ final class AddMealViewController: BaseViewController, UITextFieldDelegate {
         addMealDetailView.mealNameTextField.inputAccessoryView = toolbar
     }
     
-    private func setupInitialSelection() {
-        viewModel.inputMealTimeSelected.value = .breakfast
-        viewModel.inputMealTypeSelected.value = .homeCooked
-        viewModel.inputMealPriceEntered.value = false
-            
-        
-        addMealDetailView.updateButtonStyles(selectedMealTime: viewModel.inputMealTimeSelected.value, selectedMealType: viewModel.inputMealTypeSelected.value)
-        
-    }
-    
-    
-    private func bindViewModel() {
-        viewModel.outputFormattedDate.bind { [weak self] formattedDate in
-            guard let formattedDate = formattedDate else { return }
-            self?.dateLabel.text = formattedDate
-        }
-        
-        viewModel.outputIsCompleteButtonEnabled.bind { [weak self] isEnabled in
-            self?.completeButton.isEnabled = isEnabled
-            self?.completeButton.backgroundColor = isEnabled ? .pointGreen : .superLightGray
-            self?.completeButton.setTitleColor(isEnabled ? .white : .gray, for: .normal)
-        }
-        
-        addMealDetailView.breakfastButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
-        addMealDetailView.lunchButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
-        addMealDetailView.dinnerButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
-        addMealDetailView.snackButton.addTarget(self, action: #selector(mealTimeButtonPressed), for: .touchUpInside)
-
-        addMealDetailView.homeCookedButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
-        addMealDetailView.deliveryButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
-        addMealDetailView.diningOutButton.addTarget(self, action: #selector(mealTypeButtonPressed), for: .touchUpInside)
-        
-    }
-    
     @objc private func mealTimeButtonPressed(_ sender: UIButton) {
         switch sender {
         case addMealDetailView.breakfastButton:
-                viewModel.inputMealTimeSelected.value = .breakfast
+            viewModel.inputMealTimeSelected.value = .breakfast
         case addMealDetailView.lunchButton:
-                viewModel.inputMealTimeSelected.value = .lunch
+            viewModel.inputMealTimeSelected.value = .lunch
         case addMealDetailView.dinnerButton:
-                viewModel.inputMealTimeSelected.value = .dinner
+            viewModel.inputMealTimeSelected.value = .dinner
         case addMealDetailView.snackButton:
-                viewModel.inputMealTimeSelected.value = .snack
+            viewModel.inputMealTimeSelected.value = .snack
         default:
             break
         }
         
         addMealDetailView.updateButtonStyles(selectedMealTime: viewModel.inputMealTimeSelected.value, selectedMealType: viewModel.inputMealTypeSelected.value)
     }
-
+    
     @objc private func mealTypeButtonPressed(_ sender: UIButton) {
         switch sender {
         case addMealDetailView.homeCookedButton:
@@ -190,95 +260,34 @@ final class AddMealViewController: BaseViewController, UITextFieldDelegate {
         
         addMealDetailView.updateButtonStyles(selectedMealTime: viewModel.inputMealTimeSelected.value, selectedMealType: viewModel.inputMealTypeSelected.value)
     }
-
-    private func configureTapGesture() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showDatePicker))
-        dateView.isUserInteractionEnabled = true
-        dateView.addGestureRecognizer(tapGesture)
-    }
     
-
     
     @objc func showDatePicker() {
-        datePicker.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 216)
-        datePicker.backgroundColor = .white
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alert.view.addSubview(datePicker)
-        let okAction = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
-                self?.viewModel.inputDateSelected.value = self?.datePicker.date
-            }
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-        alert.addAction(okAction)
-        alert.addAction(cancelAction)
-        present(alert, animated: true, completion: nil)
-    }
-
-    override func configView() {
-        addMealDetailView.mealPriceTextField.delegate = self
         
-        addMealDetailView.mealPriceTextField.addTarget(self, action: #selector(textFieldDidChangeSelection), for: .editingChanged)
-        
-        completeButton.addTarget(self, action: #selector(saveMealData), for: .touchUpInside)
     }
     
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        guard let text = textField.text else {
-            viewModel.inputMealPriceEntered.value = false
+    @objc func tapCancel() {
+            self.resignFirstResponder()
+        }
+}
+
+extension AddMealViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == addMealDateView.dateTextField {
+            return false
+        } else {
+            return true
+        }
+  }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+        if textField == addMealDateView.dateTextField {
             return
         }
-        viewModel.inputMealPriceEntered.value = !text.isEmpty
-    }
-    
-    @objc private func saveMealData() {
-        viewModel.saveMealData(mealName: addMealDetailView.mealNameTextField.text, mealPrice: addMealDetailView.mealPriceTextField.text)
-//        self.navigationController?.popViewController(animated: true)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    override func configHierarchy() {
-        view.addSubviews([
-            dateView,
-            addMealDetailView,
-            completeButton
-        ])
-        
-        dateView.addSubviews([
-            dateLabel,
-            dateChevronBtn
-        ])
-    }
-    
-    override func configLayout() {
-        dateView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(36)
-            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
-            $0.height.equalTo(60)
-        }
-        
-        dateLabel.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.leading.equalToSuperview().offset(20)
-            $0.width.equalTo(200)
-        }
-        
-        dateChevronBtn.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(20)
-            $0.centerY.equalToSuperview()
-            $0.width.height.equalTo(20)
-        }
-        
-        addMealDetailView.snp.makeConstraints {
-            $0.height.equalTo(450)
-            $0.top.equalTo(dateView.snp.bottom).offset(20)
-            $0.leading.trailing.equalToSuperview().inset(20)
-        }
-        
-        completeButton.snp.makeConstraints {
-            $0.leading.bottom.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
-            $0.height.equalTo(52)
-        }
-        
-        
     }
 
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeTextField = nil
+    }
 }
